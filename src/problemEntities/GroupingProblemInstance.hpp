aea730dc1a3f553ce152b8de0a4f24f2873d756d
Julia Knura <julia.knura25@gmail.com>
2025-03-09 18:32:07 +0100

    Add visualization with OpenCV

--- src/problemEntities/GroupingProblemInstance.hpp
+++ src/problemEntities/GroupingProblemInstance.hpp
@@ -3,44 +3,50 @@
 #include <string>
 #include <optional>
 
-struct GroupingProblemParams
+namespace problem
 {
-    const size_t problemSize;
-    const size_t numberOfClasses;
-};
 
-struct MutablePoint
-{
-    double x, y;
-};
+    struct GroupingProblemParams
+    {
+        const size_t problemSize;
+        const size_t numberOfClasses;
+    };
 
-struct Point
-{
-    const double x, y;
+    struct MutablePoint
+    {
+        double x, y;
+    };
 
-    Point(double x, double y) : x(x), y(y) {};
-    Point(MutablePoint p) : x(p.x), y(p.y) {};
+    struct Point
+    {
+        const double x, y;
 
-    double euclidianDistance(const Point &other) const;
-};
+        Point(double x, double y) : x(x), y(y) {};
+        Point(MutablePoint p) : x(p.x), y(p.y) {};
 
-using ClassId = size_t;
-using GroupingProblemSolution = std::vector<ClassId>;
+        double euclidianDistance(const Point &other) const;
+    };
 
-static constexpr double BOARD_MAX_COORD{10.0};
+    using ClassId = size_t;
+    using GroupingProblemSolution = std::vector<ClassId>;
 
-class GroupingProblem
-{
-public:
-    GroupingProblem(const GroupingProblemParams &params);
-    void populate();
-    bool populate(std::vector<MutablePoint> mPoints);
-    bool isValid() const;
-    std::optional<double> evaluateSolution(const GroupingProblemSolution &solution) const;
-    const std::vector<Point> &getPoints();
-    const GroupingProblemParams &getParams();
+    static constexpr double BOARD_MAX_COORD{10.0};
 
-private:
-    GroupingProblemParams params;
-    std::vector<Point> points;
-};
+    class GroupingProblem
+    {
+    public:
+        GroupingProblem(const GroupingProblemParams &params);
+        void populate();
+        bool populate(std::vector<MutablePoint> mPoints);
+        bool isValid() const;
+        bool isSolutionValid(const GroupingProblemSolution &solution) const;
+        std::optional<double> evaluateSolution(const GroupingProblemSolution &solution) const;
+        const std::vector<Point> &getPoints() const;
+        const GroupingProblemParams &getParams() const;
+
+    private:
+        GroupingProblemParams params;
+        std::vector<Point> points;
+    };
+
+}
