ac1f1ba8f429db60d5c81fae38cf9647716559ac
Julia Knura <julia.knura25@gmail.com>
2025-03-17 21:30:41 +0100

    Refactor directory structure

---
+++ src/visualization/visualize.cpp
@@ -0,0 +1,23 @@
+#include <DrawProblem.hpp>
+
+int main()
+{
+    problem::GroupingProblemParams params{.problemSize = 100, .numberOfClasses = 10};
+    problem::GroupingProblem problem{params};
+    problem.populate();
+    visualization::drawProblemEntity(problem);
+
+    using MP = problem::MutablePoint;
+
+    problem::GroupingProblemParams paramsHappyPath = problem::GroupingProblemParams{
+        .problemSize = 10, .numberOfClasses = 3};
+    problem::GroupingProblem problem2{paramsHappyPath};
+    problem2.populate({MP{1, 2}, MP{1, 1}, MP{2, 1}, MP{5, 8}, MP{5, 7}, MP{5, 6}, MP{7, 3}, MP{7, 2}, MP{8, 5}, MP{9, 4}});
+    problem::GroupingProblemSolution solution = {0, 0, 0, 1, 1, 1, 2, 2, 2, 2};
+
+    visualization::drawProblemWithSolution(problem2, solution);
+
+    visualization::pauseApp();
+
+    return 0;
+}
